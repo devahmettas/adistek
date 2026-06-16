15222141b4f55f404a7764dd56e14a77ccd8c13e
Ahmet Taş <35.tas.ahmet@gmail.com>
2026-06-16 21:52:58 +0300

    yayın için 1 .

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,15 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: resolve(__dirname, '..'),
+    assetsDir: 'assets',
+    emptyOutDir: false,
+  },
   server: {
     proxy: {
       '/api': {
